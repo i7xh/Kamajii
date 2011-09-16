eb434c463b3407f484daefff9078bd9cbcce5370
i7xh <ixh.xiheng@gmail.com>
2011-09-16 11:10:44 +0800

    temp commit

---
+++ celeryconfig.py
@@ -0,0 +1,23 @@
+BROKER_BACKEND = 'redis'
+BROKER_HOST = '127.0.0.1'
+BROKER_PORT = 6379
+BROKER_VHOST = '12'
+
+CELERY_RESULT_BACKEND = "redis"
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+REDIS_DB = '100'
+REDIS_CONNECT_RETRY = True
+
+#CELERY_IMPORTS = ("web_config","core.middlewares.analytic","core.workers.sina_tasks")
+
+CELERY_DEFAULT_RATE_LIMIT='500/s'
+
+# Enables error e-mails.
+CELERY_SEND_TASK_ERROR_EMAILS = False
+
+# Name and e-mail addresses of recipients
+ADMINS = (
+    ("xi heng", "h@zhihu.com"),
+)
+
--- run.py
+++ run.py
@@ -22,14 +22,30 @@ sys.path.insert(0, 'lib/kombu')
 
 
 from celery import Celery
+BROKER_BACKEND = 'redis'
+BROKER_HOST = '127.0.0.1'
+BROKER_PORT = 6379
+BROKER_VHOST = '12'
+
+CELERY_RESULT_BACKEND = "redis"
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+REDIS_DB = '100'
+REDIS_CONNECT_RETRY = True
 
 
 celery = Celery("myapp")
-celery.conf.update(BROKER_URL="amqp://guest:guest@localhost:5672//")
+#celery.conf.update(BROKER_URL="amqp://guest:guest@localhost:5672//")
+celery.conf.update(BROKER_BACKEND='redis')
+celery.conf.update(BROKER_HOST='127.0.0.1')
+celery.conf.update(BROKER_PORT=6379)
+celery.conf.update(BROKER_VHOST='12')
 
+print celery.conf
 
 @celery.task
 def add(x, y):
+    print 'i am doing work'
     return x + y
 
 if __name__ == "__main__":
